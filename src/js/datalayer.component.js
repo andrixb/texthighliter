8c2f4d3e6d258981ec295268c85e5a690b4eaeab
Andrea Baldo <andrea.baldo@netcentric.biz>
2017-04-20 13:23:17 +0200

    create data model

--- src/js/datalayer.component.js
+++ src/js/datalayer.component.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     var MODULE_NAME = 'dataLayer',
@@ -6,22 +6,71 @@
 
     window[APP_NAMESPACE] = window[APP_NAMESPACE] || {};
 
-    window[APP_NAMESPACE][MODULE_NAME] = function() {
-      // It hosts module states
+    window[APP_NAMESPACE][MODULE_NAME] = function () {
+        // It hosts module states
         var _$ = {};
 
         // Initial module configuration
-        var _config = {};
+        var _config = {
+            words: '.words.text-muted'
+        };
 
-      return {
-          init: function(source, destination) {
-              _$.source = source;
-              _$.destination = destination;
-          },
-          destroy: function() {
-              _$.source = null;
-              _$.destination = null;
-          }
-      };
+        function _setInitHighlighting() {
+            _$.highlighting = '';
+        }
+
+        function _setWordsObj() {
+            _$.wordsObj = [];
+        }
+
+        function _setInitDestination() {
+            var destInitConfig = {};
+            _config.destination = window.ae.destination();
+            _config.destination.init(destInitConfig);
+        }
+
+        function _textHighlight(event) {
+            if (event.target.all) {
+                _$.highlighting += document.selection.createRange().text;
+            } else {
+                _$.highlighting += ' ' + document.getSelection();
+                _$.wordsTag = event.target.querySelectorAll(_config.words);
+                _extractWordData(_$.wordsTag);
+                _config.destination.addWords(_$.wordsObj);
+            }
+        }
+
+        function _extractWordData(element) {
+            for (var i = 0; i < element.length; i++) {
+               _$.wordsObj.push({
+                    'data-start-time': element[i].getAttribute('data-start-time'),
+                    'data-text': element[i].getAttribute('data-text'),
+                    'data-end-time': element[i].getAttribute('data-end-time')
+                });
+            }
+        }
+
+        function _addListeners() {
+            _$.source.addEventListener('mouseup', _textHighlight);
+        }
+
+        function _main() {
+            _setInitHighlighting();
+            _setWordsObj();
+            _setInitDestination();
+            _addListeners();
+        }
+
+        return {
+            init: function (source, destination) {
+                _$.source = source;
+                _$.destination = destination;
+                _main();
+            },
+            destroy: function () {
+                _$.source = null;
+                _$.destination = null;
+            }
+        };
     };
 })();
--- src/js/destination.component.js
+++ src/js/destination.component.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     var MODULE_NAME = 'destination',
@@ -6,20 +6,59 @@
 
     window[APP_NAMESPACE] = window[APP_NAMESPACE] || {};
 
-    window[APP_NAMESPACE][MODULE_NAME] = function() {
-      // It hosts module states
+    window[APP_NAMESPACE][MODULE_NAME] = function () {
+        // It hosts module states
         var _$ = {};
 
         // Initial module configuration
-        var _config = {};
+        var _defaultState = {
+            words: []
+        };
 
-      return {
-          init: function() {
+        function _getDeepCloneOfObject(object) {
+            return JSON.parse(JSON.stringify(object));
+        }
 
-          },
-          destroy: function() {
-              _$ = null;
-          }
-      };
+        function _ensureArray(item) {
+            if (item.constructor !== Array) {
+                item = [item];
+            }
+            return item;
+        }
+
+        function _setupState(state) {
+            state = state || {};
+            _$ = Object.assign({},
+                _getDeepCloneOfObject(_defaultState),
+                _getDeepCloneOfObject(state)
+            );
+        }
+
+        function _addWord(word) {
+            _$.words.push(word);
+        }
+
+        function _getWordList() {
+            return _$.words;
+        }
+
+        return {
+            init: function (initState) {
+                _setupState(initState);
+            },
+
+            getWords: _getWordList,
+
+            addWords: function (newWords) {
+                _ensureArray(newWords).forEach(function (newWord) {
+                    var word = _getDeepCloneOfObject(newWord);
+                    _addWord(word);
+                });
+            },
+
+            destroy: function () {
+                _setupState(null);
+            }
+        };
     }
-  })();
+})();
